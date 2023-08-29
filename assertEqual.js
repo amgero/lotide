84a7b5c54f05c5d90d2397dbe1d8d46d0a5f39d5
Amgero <amgero@outlook.com>
2023-08-29 20:43:26 -0300

    added conditions

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,13 @@
 //defining function
 const assertEqual = function(actual, expected) {
-
+  if (actual === expected) {
+    console.log("👍👍👍 Assertion Passed: ${actual} === ${expected}");
+  } else {
+    console.log("👎👎👎 Assertion Failed: ${actual} !== ${expected}");
+  }
 };
 
 //test
-assertEqual("LIghthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Bootcamp");
 
 assertEqual(1, 1);
