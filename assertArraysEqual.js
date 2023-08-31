efc5d712ea9d26b0e2451095dac07378447b7592
Amgero <amgero@outlook.com>
2023-08-30 21:13:10 -0300

    added assertArraysEqual, linted

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+    
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+    
+  return true;
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👍👍👍 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`👎👎👎 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // should pass
+assertArraysEqual([3, 2, 1], [1, 2, 3]); // should fail
